64816a91e36d61f686cfc0cf1b0cc0a6aa5ce93d
Rudransh Shekhar <rudranshshekhar16665@gmail.com>
2026-08-17 16:54:59 +0530

    Time: 7 ms (19.11%), Space: 43 MB (70.54%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,34 +1,40 @@
 class Solution {
     public String reorganizeString(String s) {
-        HashMap<Character,Integer> map=new HashMap<>();
-        for(int i=0; i<s.length(); i++){
-            char c= s.charAt(i);
-            if(!map.containsKey(c))map.put(c,0);
-            map.put(c,map.get(c)+1);
-        } 
-        PriorityQueue<Character> pq= new PriorityQueue<>((a, b)->{
-            return map.get(b)-map.get(a);
-        });
-        for(char key: map.keySet())pq.add(key);
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        PriorityQueue<Character> pq = new PriorityQueue<>(
+            (a, b) -> map.get(b) - map.get(a)
+        );
+
+        pq.addAll(map.keySet());
 
         StringBuilder ret = new StringBuilder();
-        while(!pq.isEmpty()){
-            if(pq.size()==1){
-                if(map.get(pq.peek())>1)return "";
-                ret.append(pq.poll());
-                break;
-            }
-            char c1=pq.poll();
-            char c2=pq.poll();
-            ret.append(c1);
-            ret.append(c2);
-            map.put(c1,map.get(c1)-1);
-            map.put(c2,map.get(c2)-1);
-            if(map.get(c1)!=0)pq.add(c1);
-            if(map.get(c2)!=0)pq.add(c2);
-            
+
+        while (pq.size() > 1) {
+            char c1 = pq.poll();
+            char c2 = pq.poll();
+
+            ret.append(c1).append(c2);
+
+            map.put(c1, map.get(c1) - 1);
+            map.put(c2, map.get(c2) - 1);
+
+            if (map.get(c1) > 0) pq.add(c1);
+            if (map.get(c2) > 0) pq.add(c2);
         }
-        if(pq.isEmpty())return ret.toString();
-        return "";  
+
+        if (!pq.isEmpty()) {
+            char c = pq.poll();
+
+            if (map.get(c) > 1) return "";
+
+            ret.append(c);
+        }
+
+        return ret.toString();
     }
 }
