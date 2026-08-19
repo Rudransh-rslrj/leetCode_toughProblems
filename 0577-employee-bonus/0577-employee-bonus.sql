f6860e2ba61ae4d65a0f760c44cb16c5004e6733
Rudransh Shekhar <rudranshshekhar16665@gmail.com>
2026-08-19 22:16:03 +0530

    Time: 1193 ms (30.92%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select t1.name as name, t2.bonus as bonus from
+select t1.name , t2.bonus  from
 Employee t1 left join Bonus t2
 on t1.empId=t2.empId
 where t2.bonus is NULL
