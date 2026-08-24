fd1649d997ba8ac9beb895a298d7745c0d8ab9a0
Rudransh Shekhar <rudranshshekhar16665@gmail.com>
2026-08-24 23:12:17 +0530

    Time: 39 ms (88.58%), Space: 47.3 MB (20.55%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,41 +1,63 @@
-class Solution {
-    
-    public int helper(int index,List<List<Integer>> adj,int dp[] ){
-        if(dp[index]!=-1)return dp[index];
-        int n=adj.get(index).size();
-        if(n==0)return dp[index]=1;
-        int max=0;
-        int k=0;
-        for(int i=0; i<n; i++){
-            k=Math.max(helper(adj.get(index).get(i),adj,dp),k);
-        }
-        return dp[index]=k+1;
-    }
+// class Solution {
+//     public int helper(int index,List<List<Integer>> adj,int dp[] ){
+//         if(dp[index]!=-1)return dp[index];
+//         int n=adj.get(index).size();
+//         if(n==0)return dp[index]=1;
+//         int max=0;
+//         int k=0;
+//         for(int i=0; i<n; i++){
+//             k=Math.max(helper(adj.get(index).get(i),adj,dp),k);
+//         }
+//         return dp[index]=k+1;
+//     }
+//     public int longestStrChain(String[] words) {
+//         HashMap<String,Integer> map = new HashMap<>();
+//         int n = words.length;
+//         List<List<Integer>> adj = new ArrayList<>();
+//         for(int i=0; i<n; i++){
+//             map.put(words[i],i);
+//             adj.add(new ArrayList<>());
+//         }
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<=words[i].length(); j++)
+//             for(char c='a'; c<='z'; c++){
+//                 String s = words[i].substring(0,j)+c+words[i].substring(j,words[i].length());
+//                 if(map.containsKey(s)){
+//                     int p = map.get(s);
+//                     adj.get(i).add(p);
+//                 }
+//             }
+//         }
+//         int dp[]=new int[n];
+//         for(int i=0; i<n; i++)dp[i]=-1;
+//         int max=1;
+//         for(int i=0; i<n; i++)if(dp[i]==-1)max=Math.max(max,helper(i,adj,dp));
+//         return max;
+//     }
+// }
 
+class Solution {
     public int longestStrChain(String[] words) {
-        HashMap<String,Integer> map = new HashMap<>();
-        int n = words.length;
-        
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0; i<n; i++){
-            map.put(words[i],i);
-            adj.add(new ArrayList<>());
-        }
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<=words[i].length(); j++)
-            for(char c='a'; c<='z'; c++){
-                String s = words[i].substring(0,j)+c+words[i].substring(j,words[i].length());
-                if(map.containsKey(s)){
-                    int p = map.get(s);
-                    adj.get(i).add(p);
+        HashMap<String, Integer> dp = new HashMap<>();
+        int ans = 1;
+
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+
+        for (String word : words) {
+            int best = 1;
+
+            for (int i = 0; i < word.length(); i++) {
+                String prev = word.substring(0, i) + word.substring(i + 1);
+
+                if (dp.containsKey(prev)) {
+                    best = Math.max(best, dp.get(prev) + 1);
                 }
             }
+
+            dp.put(word, best);
+            ans = Math.max(ans, best);
         }
-        int dp[]=new int[n];
-        for(int i=0; i<n; i++)dp[i]=-1;
-        int max=1;
-        for(int i=0; i<n; i++)if(dp[i]==-1)max=Math.max(max,helper(i,adj,dp));
-        return max;
+
+        return ans;
     }
 }
