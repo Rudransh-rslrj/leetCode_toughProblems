25111145dfe36ed5962877f8a7221d865e95e842
Rudransh Shekhar <rudranshshekhar16665@gmail.com>
2026-08-28 20:39:41 +0530

    Time: 296 ms (99.94%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,8 +1,14 @@
+
 SELECT id,
        CASE
-           WHEN id % 2 = 0 THEN LAG(student) OVER (ORDER BY id)
-           WHEN id < (SELECT MAX(id) FROM Seat)
-                THEN LEAD(student) OVER (ORDER BY id)
+           WHEN id % 2 = 0 THEN prev_student
+           WHEN next_student IS NOT NULL THEN next_student
            ELSE student
        END AS student
-FROM Seat;
+FROM (
+    SELECT id,
+           student,
+           LAG(student) OVER (ORDER BY id) AS prev_student,
+           LEAD(student) OVER (ORDER BY id) AS next_student
+    FROM Seat
+)as s;
