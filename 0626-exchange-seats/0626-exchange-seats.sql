bf95f2cf9f78da99b70a5150928672d636d46a2a
Rudransh Shekhar <rudranshshekhar16665@gmail.com>
2026-08-28 20:36:41 +0530

    Time: 382 ms (52.5%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,8 @@
+SELECT id,
+       CASE
+           WHEN id % 2 = 0 THEN LAG(student) OVER (ORDER BY id)
+           WHEN LEAD(student) OVER (ORDER BY id) IS NOT NULL
+                THEN LEAD(student) OVER (ORDER BY id)
+           ELSE student
+       END AS student
+FROM Seat;
